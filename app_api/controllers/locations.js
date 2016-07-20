190e3b0c20c3d22112815c1c5c61e7ed54fd5a02
Aung <taung@cgsh.com>
2016-07-20 10:45:06 -0400

    added changes

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -2,19 +2,42 @@ var jsonResponse = require('../controllers/sendJson');
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
 
-module.exports={
+module.exports = {
 
-    locationsListByDistance : function(req, res){
-        jsonResponse.sendJsonResponse(res, 200, {status : "success"});
+    locationsListByDistance: function (req, res) {
+        jsonResponse.sendJsonResponse(res, 200, { status: "success" });
     },
-    locationsCreate : function(req, res){
+    locationsCreate: function (req, res) {
     },
-    locationsReadOne : function () {
-    },
-    locationsUpdateOne : function(){
+    locationsReadOne: function (req, res) {
+        if (req.params && req.params.locationid) {
+            Loc
+                .findById(req.params.locationid)
+                .exec(function (err, location) {
+                    if (!location) {
+                        jsonResponse.sendJsonResponse(res, 404, {
+                            "message": "locationid not found"
+                        });
+                        return;
+                    } else if (err) {
+                        jsonResponse.sendJsonResponse(res, 404, err);
+                        return;
+                    }
+                    jsonResponse.sendJsonResponse(res, 200, location);
+
+                })
+        }
+        else {
+            jsonResponse.sendJsonResponse(res, 404, { "message": "No locationid in request" });
+        }
+
+
 
     },
-    locationsDeleteOne : function () {
-        
+    locationsUpdateOne: function () {
+
+    },
+    locationsDeleteOne: function () {
+
     }
 }
--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -1,18 +1,70 @@
-module.exports={
+var jsonResponse = require('../controllers/sendJson');
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
 
-    reviewsCreate : function(){
+module.exports = {
 
-    },
-
-    reviewsReadOne : function(){
-
-    },
-
-    reviewsUpdateOne : function(){
+    reviewsCreate: function (req, res) {
 
     },
 
-    reviewsDeleteOne : function(){
+    reviewsReadOne: function (req, res) {
+        if (req.params && req.params.locationid && req.params.reviewid) {
+            Loc
+                .findById(req.params.locationid)
+                .select('name reviews')
+                .exec(
+                function (err, location) {
+                    var response, review;
+                    if (!location) {
+                        jsonResponse.sendJsonResponse(res, 404, {
+                            "message": "locationid not found"
+                        });
+                        return;
+                    }
+                    else if (err) {
+                        jsonResponse.sendJsonResponse(res, 400, err);
+                        return;
+                    }
+
+                    if (location.reviews && location.reviews.length > 0) {
+                        review = location.reviews.id(req.params.reviewid);
+                        if (!review) {
+                            jsonResponse.sendJsonResponse(res, 404, {
+                                "message": "reviewid not found"
+                            });
+                        } else {
+                            response = {
+                                location: {
+                                    name: location.name,
+                                    id: req.params.locationid
+                                },
+                                review: review
+                            };
+                            jsonResponse.sendJsonResponse(res, 200, response);
+                        }
+
+                    } else {
+                        jsonResponse.sendJsonResponse(res, 404, {
+                            "message": "No reviews found"
+                        });
+                    }
+
+                });
+
+        } else {
+            jsonResponse.sendJsonResponse(res, 404, {
+                "message": "Not found, locationid and reviewid are both required"
+            });
+
+        }
+    },
+
+    reviewsUpdateOne: function (req, res) {
+
+    },
+
+    reviewsDeleteOne: function (req, res) {
 
     }
 }
