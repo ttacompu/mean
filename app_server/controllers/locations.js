0552b8012b05ade75ca989a2e143a59bc69702d0
Aung <taung@cgsh.com>
2016-07-25 16:50:12 -0400

    consuming web api

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,13 +1,84 @@
-module.exports.homelist = function(req, res){
-    res.render('location-list', {title : 'home'});
-
+var request = require("request");
+var apiOptions = {
+    server: "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://vast-journey-36480.herokuapp.com";
 }
 
-module.exports.locationInfo = function(req, res){
+var renderHomepage = function (req, res, responseBody) {
+    var message;
 
- res.render('location-info', {title : 'location info'});
+    if(!(responseBody instanceof Array)){
+        message =  "API lookup error";
+        responseBody = [];
+    }else{
+        if(!responseBody.length){
+            message = "No places found nearby";
+        }
+    }
+
+    res.render('locations-list', {
+        title: 'Loc8r - find a place to work with wifi',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
+        locations: responseBody,
+        message : message
+    });
+};
+
+var _formatDistance = function (distance) {
+    var numDistance, unit;
+    if (distance > 1) {
+        numDistance = parseFloat(distance).toFixed(1);
+        unit = 'km';
+    } else {
+        numDistance = parseInt(distance * 1000, 10);
+        unit = 'm';
+    }
+    return numDistance + unit;
+};
+
+module.exports.homelist = function (req, res) {
+
+    var requestOptions, path;
+    path = '/api/locations';
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "GET",
+        json: {},
+        qs: {
+            lng: -0.7992599,
+            lat: 51.378091,
+            maxDistance: 20
+        }
+    };
+    request(
+        requestOptions,
+        function (err, response, body) {
+            var i, data;
+            data = body;
+            console.log(err);
+            if (response.statusCode === 200 && data.length) {
+                for (i = 0; i < data.length; i++) {
+                    data[i].distance = _formatDistance(data[i].distance);
+                }
+            }
+            renderHomepage(req, res, data);
+        }
+    );
 }
 
-module.exports.addReview = function(req, res){
-    res.render('location-review', {title : 'Add review'});
+
+
+module.exports.locationInfo = function (req, res) {
+
+    res.render('location-info', { title: 'location info' });
+}
+
+module.exports.addReview = function (req, res) {
+    res.render('location-review', { title: 'Add review' });
 }
