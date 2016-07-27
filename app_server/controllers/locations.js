b60fd9f5cf4dba5c8cf2e81c2d9a51baac118222
Aung <taung@cgsh.com>
2016-07-27 14:47:03 -0400

    add files

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -103,8 +103,8 @@ module.exports.homelist = function (req, res) {
 
 
 
-var getLocationInfo = function(req, res, callback){
-     var requestOptions, path;
+var getLocationInfo = function (req, res, callback) {
+    var requestOptions, path;
     path = "/api/locations/" + req.params.locationid;
     requestOptions = {
         url: apiOptions.server + path,
@@ -131,25 +131,60 @@ var getLocationInfo = function(req, res, callback){
 
 module.exports.locationInfo = function (req, res) {
 
-   getLocationInfo(req, res, function(req, res, responseData){
-       renderDetailsPage(req, res, responseData);
-   })
+    getLocationInfo(req, res, function (req, res, responseData) {
+        renderDetailsPage(req, res, responseData);
+    })
 }
 
-var renderReviewForm = function(req, res, locDetail){
-    res.render('location-review-form',{
-        title : 'Review ' + locDetail.name + ' on Loc8r',
-        pageHeader : { title: 'Review ' + locDetail.name}
+var renderReviewForm = function (req, res, locDetail) {
+    res.render('location-review-form', {
+        title: 'Review ' + locDetail.name + ' on Loc8r',
+        pageHeader: { title: 'Review ' + locDetail.name },
+        error: req.query.err
 
     })
 }
 
 module.exports.addReview = function (req, res) {
-    getLocationInfo(req, res, function(req, res, responseData){
-       renderReviewForm(req, res, responseData);
-   })
+    getLocationInfo(req, res, function (req, res, responseData) {
+        renderReviewForm(req, res, responseData);
+    })
 }
 
 module.exports.doAddReview = function (req, res) {
-    
-}
+    var requestOptions, path, locationid, postdata;
+    locationid = req.params.locationid;
+    path = "/api/locations/" + locationid + '/reviews';
+
+    postdata = {
+        author: req.body.name,
+        rating: parseInt(req.body.rating, 10),
+        reviewText: req.body.review
+    };
+    requestOptions = {
+        url: apiOptions.server + path,
+        method: "POST",
+        json: postdata
+    };
+    if (!postdata.author || !postdata.rating || !postdata.reviewText) {
+        res.redirect('/location/' + locationid + '/reviews/new?err=val');
+    } else {
+
+        request(
+            requestOptions,
+            function (err, response, body) {
+                if (response.statusCode === 201) {
+                    res.redirect('/location/' + locationid);
+                } else if (response.statusCode === 400 && body.name && body.name ===
+                    "ValidationError") {
+                    res.redirect('/location/' + locationid + '/reviews/new?err=val');
+                }
+
+                else {
+                    _showError(req, res, response.statusCode);
+                }
+            }
+        );
+    }
+
+};
